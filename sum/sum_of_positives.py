f9de4086ea10bd0a31c06647bfb2c19ecf02d0bb
Daniel Gergely Banfi <daniel.banfi@evosoft.com>
2023-03-27 17:06:12 +0200

    comments to the tests

--- sum/sum_of_positives.py
+++ sum/sum_of_positives.py
@@ -10,5 +10,9 @@ def sum_of_positives(a: Any, b: Any, message: str = None) -> int:
     In case of invalid input it throws a Value error with a small comment.
     In case of any valid string message variable given, it writes it to the stdout after a "The message is:" header line.
     In case of the message contains only upper case letters, it is converted to lower case
+    :param a: required; integer, float or string
+    :param b: required; integer, float or string
+    :param message: optional; string
+    :return: integer
     """
     return a + b
--- tests/test_sum/test_sum_of_positives.py
+++ tests/test_sum/test_sum_of_positives.py
@@ -7,16 +7,27 @@ from sum.sum_of_positives import sum_of_positives
 
 class TestSumOfPositives(unittest.TestCase):
     def test_valid_int_numbers(self):
+        """
+        Basic function test of the addition of integers
+        """
         assert sum_of_positives(3, 2) == 5
         assert sum_of_positives(1, 1) == 2
 
     @unittest.skip("Step 2")
     def test_valid_float_numbers(self):
+        """
+        Test of the addition of numbers (integer and/or float),
+        the result is rounded down (after the addition)
+        """
         assert sum_of_positives(3.1, 2) == 5
         assert sum_of_positives(3.6, 2.5) == 6
 
     @unittest.skip("Step 3")
     def test_valid_float_string_numbers(self):
+        """
+        Test of the addition of any input that can be converted to number,
+        the result is rounded down (after the addition)
+        """
         assert sum_of_positives("3.1", 2) == 5
         assert sum_of_positives("3.9", 2.2) == 6
         assert sum_of_positives("3", 2) == 5
@@ -24,6 +35,9 @@ class TestSumOfPositives(unittest.TestCase):
 
     @unittest.skip("Step 4")
     def test_invalid_numbers(self):
+        """
+        Test of invalid numbers; 0 or negative
+        """
         with self.assertRaises(ValueError) as ve:
             sum_of_positives(-3, 2)
         self.assertIn("positive", str(ve.exception))
@@ -42,6 +56,17 @@ class TestSumOfPositives(unittest.TestCase):
 
     @unittest.skip("Step 5")
     def test_invalid_not_number(self):
+        """
+        Test the exception raising in case of any input is not valid
+        """
+        with self.assertRaises(ValueError) as ve:
+            sum_of_positives("3,1", 2)
+        self.assertIn("valid number", str(ve.exception))
+
+        with self.assertRaises(ValueError) as ve:
+            sum_of_positives(2, "3,1")
+        self.assertIn("valid number", str(ve.exception))
+
         with self.assertRaises(ValueError) as ve:
             sum_of_positives("a", 2)
         self.assertIn("valid number", str(ve.exception))
@@ -59,25 +84,21 @@ class TestSumOfPositives(unittest.TestCase):
         self.assertIn("number or string", str(ve.exception))
 
     @unittest.skip("Step 6")
-    def test_invalid_str_number(self):
-        with self.assertRaises(ValueError) as ve:
-            sum_of_positives("3,1", 2)
-        self.assertIn("valid number", str(ve.exception))
-
-        with self.assertRaises(ValueError) as ve:
-            sum_of_positives(2, "3,1")
-        self.assertIn("valid number", str(ve.exception))
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_message(self, mock_out):
+        """
+        Test the message output part if the message is valid
+        """
+        sum_of_positives(1, 2, "test")
+        self.assertIn("The message is:\n", mock_out.getvalue())
+        self.assertIn("test", mock_out.getvalue())
 
     @unittest.skip("Step 7")
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_message(self, mock_out):
-        sum_of_positives(1, 2, "test")
-        self.assertIn("The message is:", mock_out.getvalue())
-        self.assertIn("test", mock_out.getvalue())
-
-    @unittest.skip("Step 8")
-    @patch('sys.stdout', new_callable=io.StringIO)
-    def test_none_message(self, mock_out):
+    def test_no_message(self, mock_out):
+        """
+        Test the message output part if the message is not valid (null, empty, not string)
+        """
         sum_of_positives(1, 2)
         self.assertNotIn("The message is:", mock_out.getvalue())
 
@@ -87,19 +108,32 @@ class TestSumOfPositives(unittest.TestCase):
         sum_of_positives(1, 2, 3)
         self.assertNotIn("The message is:", mock_out.getvalue())
 
-    @unittest.skip("Step 9")
+        sum_of_positives(1, 2, [3])
+        self.assertNotIn("The message is:", mock_out.getvalue())
+
+    @unittest.skip("Step 8")
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_all_uppercase_message(self, mock_out):
-        sum_of_positives(1, 2, "UPPERCASETEST")
-        self.assertIn("The message is:", mock_out.getvalue())
-        self.assertIn("uppercasetest", mock_out.getvalue())
+        """
+        Test the message output part if the message contains only upper case letters
+        """
+        sum_of_positives(1, 2, "UPPERCASE LETTERS ONLY")
+        self.assertIn("The message is:\n", mock_out.getvalue())
+        self.assertIn("uppercase letters only\n", mock_out.getvalue())
 
-    @unittest.skip("Step 10")
+    @unittest.skip("Step 9")
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_some_uppercase_message(self, mock_out):
-        sum_of_positives(1, 2, "UPPERCASEtest")
-        self.assertIn("The message is:", mock_out.getvalue())
-        self.assertIn("UPPERCASEtest", mock_out.getvalue())
+    def test_not_all_uppercase_message(self, mock_out):
+        """
+        Test the message output part if the message contains not only upper case letters
+        """
+        sum_of_positives(1, 2, "UPPERCASE Letters")
+        self.assertIn("The message is:\n", mock_out.getvalue())
+        self.assertIn("UPPERCASE Letters\n", mock_out.getvalue())
+
+        sum_of_positives(1, 2, "no uppercase letter")
+        self.assertIn("The message is:\n", mock_out.getvalue())
+        self.assertIn("no uppercase letter\n", mock_out.getvalue())
 
 
 if __name__ == "__main__":
